28a88917f243e75a9d25256579c1e0c0fcf4a082
vanessisse <vanessasillos@gmail.com>
2022-12-27 15:23:52 -0300

    build

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { CarrinhoModule } from './carrinho/carrinho.module';
+import { CarrinhoRoutingModule } from './carrinho/carrinho-routing.module';
+
+const routes: Routes = [
+  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+  { path: "", redirectTo: "produtos", pathMatch: "full" },
+  { path: "carrinho", loadChildren: () => import("./carrinho/carrinho.module").then(m => m.CarrinhoModule) },
+  { path: 'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
+  { path: "**", component: PaginaNaoEncontradaComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
